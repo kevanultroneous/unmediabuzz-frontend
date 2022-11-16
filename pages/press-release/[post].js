cfd394d266accf213e62a1f582a0cde8a5e47ffb
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-16 12:32:56 +0530

    add 2 page (default) press-release & view detail

---
+++ pages/press-release/[post].js
@@ -0,0 +1,16 @@
+import ContainerWrraper from "@/components/common/ContainerWrraper";
+import GettingStarted from "@/components/common/GettingStarted";
+import Layout from "@/components/common/Layout";
+
+const ViewPost = () => {
+  return (
+    <Layout>
+      <ContainerWrraper>
+        <h1>ViewPost</h1>
+      </ContainerWrraper>
+      <GettingStarted />
+    </Layout>
+  );
+};
+
+export default ViewPost;
---
+++ pages/press-release/index.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/common/Layout";
+
+const PressRelease = () => {
+  return <Layout></Layout>;
+};
+
+export default PressRelease;
