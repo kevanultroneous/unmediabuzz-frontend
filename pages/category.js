598cd4bfacee2b814bdceedb99d9bb6884ac2ea9
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-17 19:54:31 +0530

    category

--- pages/category.js
+++ pages/category.js
@@ -4,10 +4,46 @@ import Layout from "@/components/common/Layout";
 import { CardModel } from "@/components/common/RecentItems";
 import CategoryHero from "@/components/PR/CategoryHero";
 import styles from "@/styles/PR/Category.module.css";
+import { useEffect, useState } from "react";
 import { Accordion, Col, Row } from "react-bootstrap";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 
 const Category = () => {
   const arry = [1, 2, 3, 4, 5];
+  const paginationData = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  ];
+  const [currentPages, setCurrentPages] = useState([]);
+  const [selectedPage, setSelectedPage] = useState(0);
+  const [gapesStore, setGapesStore] = useState([]);
+  const [gape, setGape] = useState(9);
+
+  const targetedGapes = (gape) => {
+    let gapesArray = [];
+    for (let i = 1; i <= 10; i++) {
+      gapesArray.push(gape * i);
+    }
+    return gapesArray;
+  };
+
+  useEffect(() => {
+    setGapesStore(targetedGapes(gape));
+  }, [gape]);
+
+  useEffect(() => {
+    if (selectedPage === -1) {
+      setSelectedPage(paginationData.length - 1);
+    }
+    if (
+      selectedPage === paginationData.length ||
+      gapesStore.includes(selectedPage)
+    ) {
+      setSelectedPage(0);
+    }
+  }, [gapesStore, paginationData.length, selectedPage]);
+
+  const handlePrevious = () => setSelectedPage(selectedPage - 1);
+  const handleNext = () => setSelectedPage(selectedPage + 1);
 
   return (
     <Layout>
@@ -38,19 +74,56 @@ const Category = () => {
           >
             <div className={`${styles.CategoryWrraper} CategoriesAccordion`}>
               <div className={styles.CategoryHeadingWrrap}>
-                <p className={styles.CategoryHeading}>CATEGORIES</p>
+                <p className={styles.CategoryHeading}>Category Name</p>
               </div>
-              <div className={styles.AllPrLabelWrap}>
-                <p className={styles.AllPrLabel}>All Press Release</p>
-              </div>
-              <Accordion defaultActiveKey="0">
-                {arry.map((category, index) => (
-                  <Accordion.Item eventKey={index} key={index}>
-                    <Accordion.Header>Category</Accordion.Header>
-                    <Accordion.Body></Accordion.Body>
-                  </Accordion.Item>
+              <div className={styles.SubCategoryWrraper}>
+                {arry.map((subcategory, index) => (
+                  <div
+                    key={index}
+                    className={styles.SubCategoryText}
+                  >{`Sub Category ${index + 1}`}</div>
                 ))}
-              </Accordion>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+            <div className={styles.PaginationWrraper}>
+              <div
+                className={styles.ActionButtonCover}
+                onClick={handlePrevious}
+              >
+                <BiChevronLeft />
+              </div>
+              <div>
+                {paginationData?.map((page, index) =>
+                  index == 9 ? (
+                    <>
+                      <label className={styles.PageNumber}>...</label>
+                      <label className={styles.PageNumber}>
+                        {paginationData[paginationData.length - 1]}
+                      </label>
+                    </>
+                  ) : (
+                    <>
+                      {index > 9 ? null : (
+                        <label
+                          key={index}
+                          className={`${
+                            selectedPage === index
+                              ? styles.SelectedNumber
+                              : styles.PageNumber
+                          }`}
+                        >
+                          {index + 1}
+                        </label>
+                      )}
+                    </>
+                  )
+                )}
+              </div>
+              <div className={styles.ActionButtonCover} onClick={handleNext}>
+                <BiChevronRight />
+              </div>
             </div>
           </Col>
         </Row>
