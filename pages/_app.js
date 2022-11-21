8c2756f135a2115cd48171bb0350d0696a6cbd9a
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-21 20:16:54 +0530

    custom css file chnages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/rc-paginate.css";
+import "../styles/global-variable.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
