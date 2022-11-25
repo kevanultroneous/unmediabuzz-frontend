848d7b63318cac555fb90c3e05fc0e5d1f666930
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-25 11:26:53 +0530

    not found page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/common/Layout";
+
+const NotFound = () => {
+  return (
+    <Layout title={"NO page"}>
+      <center>Not Found</center>
+    </Layout>
+  );
+};
+
+export default NotFound;
