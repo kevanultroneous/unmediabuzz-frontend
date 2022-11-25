8a40767577e07b5a93cbdbe09d5e4bac8d1aa6be
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-25 11:29:37 +0530

    404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,13 @@
+import ContainerWrraper from "@/components/common/ContainerWrraper";
 import GettingStarted from "@/components/common/GettingStarted";
 import Layout from "@/components/common/Layout";
 
 const NotFound = () => {
   return (
     <Layout title={"NO page"}>
+      <ContainerWrraper>
+        <h1>No Page Found</h1>
+      </ContainerWrraper>
       <GettingStarted />
     </Layout>
   );
