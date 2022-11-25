1886d7a787c270a1ea220c86b139a06b9cdafe2e
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-25 11:28:04 +0530

    no found update

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,10 @@
+import GettingStarted from "@/components/common/GettingStarted";
 import Layout from "@/components/common/Layout";
 
 const NotFound = () => {
   return (
     <Layout title={"NO page"}>
-      <center>Not Found</center>
+      <GettingStarted />
     </Layout>
   );
 };
